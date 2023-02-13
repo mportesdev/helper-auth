fc271aeb34f35c96a0054ad5488d301f22ef2fb8
Michal Porteš <michalportes1@gmail.com>
2023-02-13 09:56:37 +0100

    Rename attributes to indicate they are private

--- helper_auth/__init__.py
+++ helper_auth/__init__.py
@@ -18,25 +18,25 @@ class HelperAuth(AuthBase):
         header="Authorization",
         cache_token=False,
     ):
-        self.command = _ensure_list(command)
-        self.key = key
-        self.prefix = prefix
-        self.header = header
-        self.cache_token = cache_token
+        self._command = _ensure_list(command)
+        self._key = key
+        self._prefix = prefix
+        self._header = header
+        self._cache_token = cache_token
         self._token = None
 
     def __call__(self, request):
-        request.headers[self.header] = f"{self.prefix}{self._get_token()}"
+        request.headers[self._header] = f"{self._prefix}{self._get_token()}"
         return request
 
     def _get_token(self):
         if self._token is not None:
             return self._token
         helper_output = subprocess.run(
-            self.command, capture_output=True, check=True, encoding="utf-8", text=True
+            self._command, capture_output=True, check=True, encoding="utf-8", text=True
         ).stdout
         token = self._token_from_helper_output(helper_output)
-        if self.cache_token:
+        if self._cache_token:
             self._token = token
         return token
 
@@ -46,9 +46,9 @@ class HelperAuth(AuthBase):
                 key, value = line.split("=", maxsplit=1)
             except ValueError:
                 continue
-            if key.strip() == self.key:
+            if key.strip() == self._key:
                 return value.strip()
-        raise KeyError(f"helper did not provide the key {self.key!r}")
+        raise KeyError(f"helper did not provide the key {self._key!r}")
 
 
 def _ensure_list(command):
