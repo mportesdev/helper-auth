4519037acf91d078e1f5e3104f5ec851ed68968b
Michal Porteš <michalportes1@gmail.com>
2023-02-15 12:36:30 +0100

    Incorporate expected values into test cases

--- tests/test_ensure_list.py
+++ tests/test_ensure_list.py
@@ -1,19 +1,8 @@
 from pytest_cases import parametrize_with_cases
-from pytest_cases.filters import id_has_suffix, id_match_regex
 
 from helper_auth import _ensure_list
 
 
-@parametrize_with_cases("command", filter=~id_match_regex(".*_arg"))
-def test_ensure_list_without_args(command):
-    assert _ensure_list(*command) == ["helper"]
-
-
-@parametrize_with_cases("command", filter=id_has_suffix("_one_arg"))
-def test_ensure_list_with_one_arg(command):
-    assert _ensure_list(*command) == ["helper", "--option"]
-
-
-@parametrize_with_cases("command", filter=id_has_suffix("_more_args"))
-def test_ensure_list_with_more_args(command):
-    assert _ensure_list(*command) == ["helper", "--option", "arg"]
+@parametrize_with_cases("case")
+def test_ensure_list(case):
+    assert _ensure_list(*case.command) == case.expected
--- tests/test_ensure_list_cases.py
+++ tests/test_ensure_list_cases.py
@@ -1,33 +1,60 @@
+from collections import namedtuple
 from pathlib import Path
+
+_case = namedtuple("_case", "command expected")
 
 
 def case_simple_string():
-    return ("helper",)
+    return _case(
+        command=("helper",),
+        expected=["helper"],
+    )
 
 
 def case_simple_path_like():
-    return (Path("helper"),)
+    return _case(
+        command=(Path("helper"),),
+        expected=["helper"],
+    )
 
 
 def case_single_string_one_arg():
-    return ("helper --option",)
+    return _case(
+        command=("helper --option",),
+        expected=["helper", "--option"],
+    )
 
 
 def case_single_string_more_args():
-    return ("helper --option arg",)
+    return _case(
+        command=("helper --option arg",),
+        expected=["helper", "--option", "arg"],
+    )
 
 
 def case_separate_string_one_arg():
-    return "helper", "--option"
+    return _case(
+        command=("helper", "--option"),
+        expected=["helper", "--option"],
+    )
 
 
 def case_separate_string_more_args():
-    return "helper", "--option", "arg"
+    return _case(
+        command=("helper", "--option", "arg"),
+        expected=["helper", "--option", "arg"],
+    )
 
 
 def case_separate_path_like_one_arg():
-    return Path("helper"), "--option"
+    return _case(
+        command=(Path("helper"), "--option"),
+        expected=["helper", "--option"],
+    )
 
 
 def case_separate_path_like_more_args():
-    return Path("helper"), "--option", "arg"
+    return _case(
+        command=(Path("helper"), "--option", "arg"),
+        expected=["helper", "--option", "arg"],
+    )
